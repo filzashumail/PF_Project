8e43bb858aac9ce1a12940df7212cda7ed51eb0f
Filza Shumail <filzashumail@gmail.com>
2026-05-08 14:59:38 +0500

    initializations

--- PF_P.cpp
+++ PF_P.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>   
+#include <ctime>     
+#include <iomanip>   
+
 using namespace std;
 
+
+
+
+
+
+int main (){
+
+srand(time(0));
+int*** CampusGrid = nullptr;
+int numDepts, numFloors, numRooms, numStudents=0, step=0, currentFloor=0, currentDept=0;
+char** names     = nullptr;
+int*   ids       = nullptr;
+float* gpas      = nullptr;
+int*   statuses  = nullptr;
